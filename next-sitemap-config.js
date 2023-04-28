706e905f0492d25d8e254c3d7c2c13274e727f73
Pavan <pavansargar36@gmail.com>
2023-04-28 16:15:57 +0530

    feat: :mag: Improved SEO added XML Sitemap

--- next-sitemap-config.js
+++ next-sitemap-config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
     siteUrl:  'https://waystoweb.com',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
+    generateRobotsTxt: true,
+    outDir: "./.next"
   }

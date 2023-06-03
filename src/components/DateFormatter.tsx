ab63fde3b31fe44db59db41cc4c44735fad2f641
Pavan <pavansargar36@gmail.com>
2023-06-03 19:16:48 +0530

    fixed contrast for seo

--- src/components/DateFormatter.tsx
+++ src/components/DateFormatter.tsx
@@ -8,7 +8,7 @@ type Props = {
 const DateFormatter = ({ dateString }: Props) => {
   const date = parseISO(dateString);
   return (
-    <time className="text-slate-500" dateTime={dateString}>
+    <time className="text-slate-600 text-sm" dateTime={dateString}>
       {format(date, "LLLL d, yyyy")}
     </time>
   );
